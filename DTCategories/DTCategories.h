6f7623efd72956a3bb4ea3603571e2a68fd4c0dd
陈芳军 <84005870@qq.com>
2019-06-19 10:51:34 +0800

    no message

--- DTCategories/DTCategories.h
+++ DTCategories/DTCategories.h
@@ -14,6 +14,5 @@
 #import "UIColor+DTAdd.h"
 #import "NSString+DTAdd.h"
 #import "CALayer+DTAdd.h"
-#import "UITextView+DTAdd.h"
 
 #endif /* DTCategories_h */
--- Demo/DTCategoriesDemo/DTCategoriesDemo/ViewController.m
+++ Demo/DTCategoriesDemo/DTCategoriesDemo/ViewController.m
@@ -23,19 +23,9 @@
 }
 
 - (void)doRightAction {
-    [self.textView setPlaceholder:@"天上飞过一群天使"];
 }
 
 - (void)doLeftAction {
-    UITextView *textView = [UITextView new];
-    self.textView = textView;
-    textView.backgroundColor = [UIColor yellowColor];
-    textView.frame = CGRectMake(15, 200, self.view.bounds.size.width-30, 200);
-    [self.view addSubview:textView];
-    [textView setPlaceholder:@"请输入文字.."];
-    textView.delegate = self;
-    
-    [textView.text addObserver:self forKeyPath:@"length" options:NSKeyValueObservingOptionNew context:nil];
 }
 
 - (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSKeyValueChangeKey,id> *)change context:(void *)context {
